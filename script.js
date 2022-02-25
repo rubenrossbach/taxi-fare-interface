d62a5670d7a5e731a800966d9690fc027f96a83e
Ruben Rossbach <ruben.rossbach@gmx.net>
2022-02-25 12:29:55 +0100

    modified script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxifareapi-2chu5w4mva-ey.a.run.app/predict';
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
